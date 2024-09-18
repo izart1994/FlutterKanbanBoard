2526ff3811e80fd3fa9610519fef3f3c4ade4ce7
Muhd Izzat <izart1994@gmail.com>
2024-09-18 15:19:29 +0800

    Update board_provider.dart

--- lib/Provider/board_provider.dart
+++ lib/Provider/board_provider.dart
@@ -130,8 +130,8 @@ class BoardProvider extends ChangeNotifier {
       scrollingRight = true;
       if (board.boardScrollConfig == null) {
         log("HEREEEE");
-        await board.controller.animateTo(board.controller.offset - 1,
-            duration: const Duration(milliseconds: 10), curve: Curves.linear);
+        await board.controller.animateTo(board.controller.offset + 1,
+            duration: const Duration(milliseconds: 1), curve: Curves.linear);
       } else {
         await board.controller.animateTo(
             board.boardScrollConfig!.offset + board.controller.offset,
